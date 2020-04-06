7b214ed418c1652744cb9854eaebe0ebad078df1
jasonkim7288 <jason.kim7288@gmail.com>
2020-04-06 13:43:28 +1000

    more modify

--- ex5.rb
+++ ex5.rb
@@ -1,6 +1,4 @@
-def divisible_by_5?(num)
-    return num % 5 == 0
-end
+require_relative('ex4')
 
 def grouping_divisible_by_5(num_array)
     return num_array.inject([[], []]) {|result, num| divisible_by_5?(num) ? (result[0].push(num)) : (result[1].push(num)); result}
